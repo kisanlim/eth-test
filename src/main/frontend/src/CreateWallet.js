0e6eea544311c4d6c7875c95b654b7a0a3e65bfc
sank2168 <sank2168@axiasoft.com>
2023-06-08 14:53:43 +0900

    eth service,controller 추가

--- src/main/frontend/src/CreateWallet.js
+++ src/main/frontend/src/CreateWallet.js
@@ -8,8 +8,9 @@ function CreateWallet(){
     const generateNewAddress = () => {
         axios.get('/api/v1/eth/create') // API 엔드포인트에 맞게 경로를 수정해야 합니다.
             .then(response => {
-                const address = response.data.walletFileName; // 응답에서 주소를 가져옵니다.
-                setWalletAddress(address); // 가져온 주소를 상태로 설정합니다.
+                // const address = response.data.walletFileName; // 응답에서 주소를 가져옵니다.
+                // setWalletAddress(address); // 가져온 주소를 상태로 설정합니다.
+                console.log("response : " + response);
             })
             .catch(error => {
                 console.error('주소 생성에 실패했습니다.', error);
